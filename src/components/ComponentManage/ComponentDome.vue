0eec1af181bb4e2ab476d24f369fd393fe5d314c
World Hello <wangyizhujp@126.com>
2018-12-24 20:20:25 +0800

    tebn

--- src/components/ComponentManage/ComponentDome.vue
+++ src/components/ComponentManage/ComponentDome.vue
@@ -1,5 +1,28 @@
 <template>
     <div>
-        这是组件操作
+        <p>
+            <demo :msg='msgData' v-bind="msgDome" :math='mathData' :fn="myFunction"></demo>
+        </p>
+
     </div>
 </template>
+<script>
+    import Demo from './ChildComponents.vue'
+    export default {
+        data() {
+            return {
+                msgData: '',
+                mathData: 2,
+                msgDome: { subjectId: '2', status: '0' }
+            }
+        },
+        methods: {
+            myFunction() {
+                alert("hello")
+            }
+        },
+        components: {
+            Demo
+        }
+    }
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,47 +1,62 @@
 <template>
-    <div>
-        <h2>VUE学习概念</h2>
-        <h3>组件</h3>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank" rel="noopener">组件1</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">组件2</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">组件3</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">组件4</a></li>
-            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">组件5</a></li>
-        </ul>
-        <h3>Vue-router（路 由）</h3>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank" rel="noopener">路由1</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">路由2</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">路由3</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">路由4</a></li>
-            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">路由5</a></li>
-        </ul>
-        <h3>Vuex</h3>
-        <h3>Vue-Cli（脚手架）</h3>
-        <h3>Vue-Test-Utils（组件测试）</h3>
-        <h3>WebPack</h3>
-        <h3>TypeScript</h3>
-    </div>
+  <div>
+    <h2>VUE学习概念</h2>
+    <h3>组件</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">组件1</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">组件2</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">组件3</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">组件4</a>
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">组件5</a>
+      </li>
+    </ul>
+    <h3>Vue-router（路 由）</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">路由1</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">路由2</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">路由3</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">路由4</a>
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">路由5</a>
+      </li>
+    </ul>
+    <h3>Vuex</h3>
+    <h3>Vue-Cli（脚手架）</h3>
+    <h3>Vue-Test-Utils（组件测试）</h3>
+    <h3>WebPack</h3>
+    <h3>TypeScript</h3>
+  </div>
 </template>
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
-
-    ul {
-
-        list-style-type: none;
-        padding: 0;
-    }
-
-    li {
-
-        display: inline-block;
-        margin: 0 10px;
-    }
-
-    a {
-        color: #42b983;
-    }
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
